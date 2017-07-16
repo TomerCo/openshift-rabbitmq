97e725c5e786dbc4fff2d3918d9a9c2e300d1dec
Tomer Cohen <tomercoh@amdocs.com>
2017-07-16 13:29:26 +0300

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -133,13 +133,6 @@ fi
 
 if [ "${RABBITMQ_ERLANG_COOKIE:-}" ]; then
 	cookieFile='/var/lib/rabbitmq/.erlang.cookie'
-	if [ -e "$cookieFile" ]; then
-		if [ "$(cat "$cookieFile" 2>/dev/null)" != "$RABBITMQ_ERLANG_COOKIE" ]; then
-			echo >&2
-			echo >&2 "warning: $cookieFile contents do not match RABBITMQ_ERLANG_COOKIE"
-			echo >&2
-		fi
-	else
 		echo "$RABBITMQ_ERLANG_COOKIE" > "$cookieFile"
 		chmod 600 "$cookieFile"
 	fi
