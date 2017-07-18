b8f2bbc6c4bd6bb475181bf7e420d99a838effd6
Tomer Cohen <tomercoh@amdocs.com>
2017-07-18 13:57:16 +0300

    fix

--- scripts/add_admin_user.sh
+++ scripts/add_admin_user.sh
@@ -11,7 +11,7 @@ echo "Adding user $1...."
 /usr/sbin/rabbitmqctl list_user_permissions  $1 &> /dev/null
 if [ $? -eq 70 ]; then
  #user does not exist
-	/usr/sbin/rabbitmqctl $1 $2
+	/usr/sbin/rabbitmqctl add_user $1 $2
 	if [ $? -ne 0 ]; then
 		echo "ERROR - failed to add admin user" 
 		exit 1
